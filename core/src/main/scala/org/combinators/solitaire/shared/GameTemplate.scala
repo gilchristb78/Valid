4de96d7b15672bc0de7101c320dd836fb0e4f513
George Heineman <heineman@cs.wpi.edu>
2017-09-12 22:26:00 -0400

    Continuing to improve documentation. Starting with GameTemplate, which is now done. Note to get a working 'SolvableGame, I have to work on my 'autoMovesAvailable' generation capability.

--- core/src/main/scala/org/combinators/solitaire/shared/GameTemplate.scala
+++ core/src/main/scala/org/combinators/solitaire/shared/GameTemplate.scala
@@ -19,6 +19,11 @@ trait GameTemplate {
   // domain model elements for game defined here...
   lazy val tableauType = Variable("TableauType")
 
+  /**
+    * Purpose of this combinator is to declare base state for any solitaire variation, namely
+    * that it starts with no core elements (Tableau, Foundation, Reserve, ...) nor Layout,
+    * nor Rules.
+    */
   @combinator object NewEmptySolitaire {
     def apply(): Solitaire = new Solitaire()
     val semanticType: Type =
@@ -29,6 +34,12 @@ trait GameTemplate {
         'Solitaire ('Rules ('None))
   }
 
+  /**
+    * Class for constructing Tableau from n Columns.
+    *
+    * @param n
+    * @param nAsType
+    */
   class NColumnTableau(n: Int, nAsType: Type) {
     def apply(): Tableau = {
       val t = new Tableau()
@@ -40,6 +51,12 @@ trait GameTemplate {
     val semanticType: Type = 'Tableau ('Valid :&: nAsType :&: 'Column)
   }
 
+  /**
+    * Class for constructing Tableau from n Piles.
+    *
+    * @param n
+    * @param nAsType
+    */
  class NPileTableau(n: Int, nAsType: Type) {
     def apply(): Tableau = {
       val t = new Tableau()
@@ -51,21 +68,18 @@ trait GameTemplate {
     val semanticType: Type = 'Tableau ('Valid :&: nAsType :&: 'Pile)
   }
 
-
-  // generic 8-column tableau
+  /**
+    * Specialized combinators for common scenarios of tableaus from piles and columns.
+    */
   @combinator object EightColumnTableau extends NColumnTableau(8, 'Eight)
-
-  // generic 4-column tableau
-  @combinator object FourColumnTableau extends NColumnTableau(4, 'Four)
-
-  // generic 8-pile tableau
+  @combinator object FourColumnTableau extends NColumnTableau(4,  'Four)
   @combinator object EightPileTableau extends NPileTableau(8, 'Eight)
-
-  // generic 4-column tableau
   @combinator object FourPileTableau extends NPileTableau(4, 'Four)
 
 
-  // Deck on the left and four columns to the right
+  /**
+    * Common layout for solitaire games with just Stcok on left and tableau on right.
+    */
   @combinator object StockTableauLayout {
     def apply(): Layout = {
       val lay = new Layout()
@@ -78,11 +92,19 @@ trait GameTemplate {
     val semanticType: Type = 'Layout ('Valid :&: 'StockTableau)
   }
 
-  // Standard Layout with Tableau below a Reserve (Left) and Foundation (Right)
+  /**
+   * Standard Layout with Tableau below a Reserve (Left) and Foundation (Right)
+   *
+   * Suggestion: Make a Deck as part of domain model, so it can be used to provide
+   * these necessary constants.
+   *
+   */
   @combinator object FoundationReserveTableauLayout {
     def apply(): Layout = {
       val lay = new Layout()
 
+      // For record, start by assuming a standard deck size of (73x97) which can be
+      // changed with some effort, or by adding parameter for decks to use.
       // width = 73
       // height = 97
 
@@ -96,7 +118,11 @@ trait GameTemplate {
     val semanticType: Type = 'Layout ('Valid :&: 'FoundationReserveTableau)
   }
 
-  // create three separate blocks based on the domain model.
+  /**
+    * Default initialization for a solitaire plugin requires initializing the
+    * Model, View and then Control sections. Once done, then provide specific initialization
+    * for the game (i.e., initial deal).
+    */
   @combinator object Initialization {
     def apply(minit: Seq[Statement],
       vinit: Seq[Statement],
@@ -111,7 +137,19 @@ trait GameTemplate {
   // these next three functions help map domain model to Java code
   // not entirely sure this is 'GUI' stuff.
 
-  /** Field Declarations: Used to create fields in subclass. */
+  /**
+    * Within a solitaire plugin, one needs to construct model elements that are added to the
+    * model, and then view widgets to realize these model elements on the screen. This function
+    * adds array of fields for both model elements and view widgets.
+    *
+    * Note: prefix is used when constructing these elements to ensure unique names.
+    *
+    * @param name
+    * @param modelType
+    * @param viewType
+    * @param num
+    * @return
+    */
   def fieldGen(name:String, modelType:String, viewType:String, num:Int):Seq[FieldDeclaration] = {
      Java(s"""
            |public static final String field${name}sPrefix = "$name";
@@ -122,7 +160,17 @@ trait GameTemplate {
            .map(_.asInstanceOf[FieldDeclaration])
   }
 
-  /** Insert code for basic controllers on a widget. */
+  /**
+    * Within a solitaire plugin, one needs to register controllers with the given view widgets
+    * constructed earlier.
+    *
+    * The default adapters (for mouse motion and undo) are registered, and the designated
+    * controller is passed in as a parameter to be used and associated with the widgets.
+    *
+    * @param viewName
+    * @param contName
+    * @return
+    */
   def controllerGen(viewName:String, contName:String) : Seq[Statement] = {
      Java(s"""
            |$viewName.setMouseMotionAdapter (new SolitaireMouseMotionAdapter (this));
@@ -132,7 +180,16 @@ trait GameTemplate {
 
   }
 
-  /** Useful for constructing controller initializations. */
+  /**
+    * Convenient function to iterate over views for a given container (i.e., Reserve or Tableau)
+    * and assign controllers based on the given type.
+    *
+    * Iterates over variable 'j' just to be different ;)
+    * @param cont
+    * @param viewName
+    * @param contName
+    * @return
+    */
   def loopControllerGen(cont: Container, viewName : String, contName:String): Seq[Statement] = {
         val nc = cont.size()
         val inner = controllerGen(viewName + "[j]", contName)
@@ -143,28 +200,20 @@ trait GameTemplate {
            |}""".stripMargin).statements()
      }
 
-  def constructGen(modelName:String, viewName:String, typ:String):Seq[Statement] = {
-    Java(s"""
-           |$modelName = new $typ(${modelName}Prefix + (j+1));
-           |addModelElement ($modelName);
-           |$viewName = new ${typ}View($modelName);
-           |}""".stripMargin).statements()
 
-  }
-
-  /** Generate a deck which has special requirements. */
-  def deckGen (modelName:String, viewName:String):Seq[Statement] = {
-     Java(
-        s"""|// Basic start of pretty much any solitaire game that requires a deck.
-            |$modelName = new Deck ("deck");
-            |$viewName = new DeckView($modelName);
-            |int seed = getSeed();
-            |$modelName.create(seed);
-            |addModelElement ($modelName);
-            |""".stripMargin).statements()
-  }
-
-  /** Useful for constructing view initializations. */
+  /**
+    * Convenient function to iterate over the construction of individual entity and view widgets.
+    *
+    * Note: Not easy to break this out into a helper function, because of the variable 'j' used
+    * to iterate over the elements, and the need to compute a unique name (based on the prefix)
+    * for each of the j elements.
+    *
+    * @param cont
+    * @param modelName
+    * @param viewName
+    * @param typ
+    * @return
+    */
   def loopConstructGen(cont: Container, modelName: String, viewName : String, typ:String): Seq[Statement] = {
         val nc = cont.size()
         Java(
@@ -176,19 +225,40 @@ trait GameTemplate {
            |}""".stripMargin).statements()
      }
 
+  /**
+    * Generate statements that constructs a single Deck (and associated Widget view), adds it to
+    * the model and initializes it to be ready.
+    *
+    * @param modelName
+    * @param viewName
+    * @return
+    */
+  def deckGen (modelName:String, viewName:String):Seq[Statement] = {
+    Java(
+      s"""|// Basic start of pretty much any solitaire game that requires a deck.
+          |$modelName = new Deck ("deck");
+          |$viewName = new DeckView($modelName);
+          |int seed = getSeed();
+          |$modelName.create(seed);
+          |addModelElement ($modelName);
+          |""".stripMargin).statements()
+  }
+
+
+  /**
+    * Primary combinator which constructs the subclass of Solitaire to represent the
+    * class for a variation.
+    */
   @combinator object MainGame {
 
-    def apply(rootPackage: Name,
-      nameParameter: SimpleName,
-      extraImports: Seq[ImportDeclaration],
-      extraFields: Seq[FieldDeclaration],
+    def apply(rootPackage: Name, nameParameter: SimpleName,
+      extraImports: Seq[ImportDeclaration], extraFields: Seq[FieldDeclaration],
       extraMethods: Seq[MethodDeclaration],
       initializeSteps: Seq[Statement],
       winParameter: Seq[Statement]): CompilationUnit = {
 
       shared.java.GameTemplate
-        .render(
-          rootPackage = rootPackage,
+        .render(rootPackage = rootPackage,
           extraImports = extraImports,
           nameParameter = nameParameter,
           extraFields = extraFields,
@@ -199,16 +269,18 @@ trait GameTemplate {
     }
 
     val semanticType: Type =
-      'RootPackage =>:
-        'NameOfTheGame =>:
-        'ExtraImports =>:
-        'ExtraFields =>:
+      'RootPackage =>: 'NameOfTheGame =>:
+        'ExtraImports =>: 'ExtraFields =>:
         'ExtraMethods =>:
         'Initialization :&: 'NonEmptySeq =>:
         'WinConditionChecking :&: 'NonEmptySeq =>:
         'SolitaireVariation
   }
 
+  /**
+    * Synthesizes a solvable game if there are 'AvailableMoves associated with
+    * the 'ExtraMethods
+    */
   @combinator object SolvableGame {
 
     def apply(rootPackage: Name,
