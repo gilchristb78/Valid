6be85cf756c291af872f862a9d25620788763fd4
dcicc4 <dcicc4@github.com>
2018-09-18 10:04:18 -0400

    Fan Except for move to foundation

---
+++ src/main/scala/org/combinators/solitaire/fan/Fan.scala
@@ -0,0 +1,32 @@
+package org.combinators.solitaire.fan
+
+import com.github.javaparser.ast.CompilationUnit
+import javax.inject.Inject
+import org.combinators.cls.git._
+import org.combinators.cls.interpreter.ReflectedRepository
+import org.combinators.cls.types.Constructor
+import org.combinators.solitaire.shared.cls.Synthesizer
+import org.combinators.templating.persistable.JavaPersistable._
+import org.webjars.play.WebJarsUtil
+import play.api.inject.ApplicationLifecycle
+
+
+class Fan @Inject()(webJars: WebJarsUtil, applicationLifecycle: ApplicationLifecycle) extends InhabitationController(webJars, applicationLifecycle) with RoutingEntries {
+
+  val solitaire = new domain.fan.Domain()
+
+  lazy val repository = new FanDomain(solitaire) with controllers {}
+
+  lazy val Gamma = repository.init(ReflectedRepository(repository, classLoader = this.getClass.getClassLoader), solitaire)
+
+
+  lazy val combinatorComponents = Gamma.combinatorComponents
+
+  lazy val targets: Seq[Constructor] = Synthesizer.allTargets(solitaire)
+
+  lazy val results: Results =
+    EmptyInhabitationBatchJobResults(Gamma).addJobs[CompilationUnit](targets).compute()
+
+  lazy val controllerAddress: String = solitaire.name.toLowerCase
+
+}
---
+++ src/main/scala/org/combinators/solitaire/fan/controllers.scala
@@ -0,0 +1,90 @@
+package org.combinators.solitaire.fan
+
+import com.github.javaparser.ast.expr.SimpleName
+import com.github.javaparser.ast.stmt.Statement
+import domain._
+import org.combinators.cls.interpreter.ReflectedRepository
+import org.combinators.cls.types.Type
+import org.combinators.cls.types.syntax._
+import org.combinators.generic
+import org.combinators.solitaire.shared
+import org.combinators.solitaire.shared._
+import org.combinators.templating.twirl.Java
+
+/** Defines Archway's controllers and their behaviors.
+  *
+  * Every controller requires definitions for three actions:
+  *   - Click (no dragging, like clicking on a deck to deal more cards)
+  *   - Press (click, drag, release)
+  *   - Release (release after press)
+  *
+  * Either a rule must be associated with an action, or the action must be
+  * explicity ignored. See ArchwayRules in game.scala.
+  */
+trait controllers extends shared.Controller with shared.Moves with GameTemplate with generic.JavaCodeIdioms  {
+
+  // dynamic combinators added as needed
+  override def init[G <: SolitaireDomain](gamma : ReflectedRepository[G], s:Solitaire) :
+  ReflectedRepository[G] = {
+    var updated = super.init(gamma, s)
+    println (">>> Archway Controller dynamic combinators.")
+
+    updated = createMoveClasses(updated, s)
+    updated = createDragLogic(updated, s)
+    updated = generateMoveLogic(updated, s)
+    updated = generateExtendedClasses(updated, s)
+
+    updated = updated
+      .addCombinator (new IgnoreClickedHandler(column))
+      .addCombinator (new SingleCardMoveHandler(column))
+
+
+    updated = updated
+      .addCombinator (new IgnorePressedHandler(pile))
+      .addCombinator (new IgnoreClickedHandler(pile))
+      .addCombinator (new IgnoreReleasedHandler(pile))
+
+    updated = updated
+      .addCombinator (new IgnoreClickedHandler(deck))
+      .addCombinator (new IgnoreReleasedHandler(deck))
+
+    updated = updated
+      .addCombinator(new DealToTableauHandlerLocal())
+
+    updated = createWinLogic(updated, s)
+
+    // move these to shared area
+    updated = updated
+      .addCombinator (new DefineRootPackage(s))
+      .addCombinator (new DefineNameOfTheGame(s))
+      .addCombinator (new ProcessModel(s))
+      .addCombinator (new ProcessView(s))
+      .addCombinator (new ProcessControl(s))
+      .addCombinator (new ProcessFields(s))
+
+    updated
+  }
+
+    /**
+      * When dealing card(s) from the stock to all elements in Tableau
+      * If deck is empty, then reset.
+      * NOTE: How to make this more compositional?
+      */
+    class DealToTableauHandlerLocal() {
+      def apply(): (SimpleName, SimpleName) => Seq[Statement] = (widget, ignore) => {
+        Java(s"""|{Move m = new DealDeck(theGame.deck, theGame.tableau);
+                 |if (m.doMove(theGame)) {
+                 |   theGame.pushMove(m);
+                 |   // have solitaire game refresh widgets that were affected
+                 |   theGame.refreshWidgets();
+                 |   return;
+                 |}}""".stripMargin
+        ).statements()
+      }
+
+      val semanticType: Type = drag(drag.variable, drag.ignore) =>: controller(deck, controller.pressed)
+    }
+
+}
+
+
