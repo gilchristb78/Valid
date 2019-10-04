7bb3d98e2dda4641397e5a29feb9d302cd99e4ce
dandaman2 <dfduff@wpi.edu>
2019-10-03 20:32:27 -0400

    Simplesimon and Routes Changes Addition

---
+++ src/main/scala/org/combinators/solitaire/simplesimon/package.scala
@@ -0,0 +1,38 @@
+package org.combinators.solitaire
+
+import org.combinators.solitaire.domain._
+
+
+package object simplesimon extends variationPoints {
+
+//  override def getDeal: Seq[DealStep] = {
+//    var colNum:Int = 0
+//    var dealSeq:Seq[DealStep] = Seq()// doesn't like me declaring it without initializing
+//    //first four piles get 5 face down cards
+//    for (colNum <- 0 to 3) {
+//      dealSeq = dealSeq :+ DealStep(ElementTarget(Tableau, colNum), Payload(faceUp = false, numCards = 5))
+//    }
+//    //the rest get 4 face down cards
+//    for (colNum <- 4 to 9) {
+//      dealSeq = dealSeq :+ DealStep(ElementTarget(Tableau, colNum), Payload(faceUp = false, numCards = 4))
+//    }
+//    //each pile gets a face up card
+//    colNum = 0
+//    for (colNum <- 0 to 9) {
+//      dealSeq = dealSeq :+ DealStep(ElementTarget(Tableau, colNum), Payload(faceUp = true, numCards = 1))
+//    }
+//    dealSeq
+//  }
+
+  val simplesimon:Solitaire = {
+    Solitaire(name = "Simplesimon",
+      structure = structureMap,
+      layout = Layout(map),
+      deal = getDeal,
+      specializedElements = Seq.empty,
+      moves = Seq(tableauToTableauMove, tableauToFoundationMove),  //deckDealMove before flipmove and in varpoints. End with flipmove
+      logic = BoardState(Map(Foundation -> 52)),
+      solvable = false
+    )
+  }
+}
