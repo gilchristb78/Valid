2232221bb713e15ae64eacca505fd8b7c41d3f8e
gilchristb78 <gilchristb78@gmail.com>
2023-02-17 08:18:26 +0000

    Travis build update,  Valid directory

---
+++ Valid/archway/AcesUpPileView.java
@@ -0,0 +1,10 @@
+package org.combinators.solitaire.archway;
+
+import ks.common.view.*;
+
+public class AcesUpPileView extends PileView {
+
+    public AcesUpPileView(AcesUpPile element) {
+        super(element);
+    }
+}
